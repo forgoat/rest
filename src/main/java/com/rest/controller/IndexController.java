7c6b6d4dddbd30de7612cdff6abc0264a8c85eb9
father <1359068985@qq.com>
2018-12-21 12:35:46 +0800

    url访问

--- src/main/java/com/rest/controller/IndexController.java
+++ src/main/java/com/rest/controller/IndexController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping(value = "/index")
     public String index(){
-        return "index";
+        return "index.html";
     }
 }
--- src/main/java/com/rest/controller/UserController.java
+++ src/main/java/com/rest/controller/UserController.java
@@ -6,6 +6,7 @@ import com.rest.service.StudentService;
 import com.rest.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
