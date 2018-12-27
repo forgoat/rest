7d89d30e031d594e1d070b63f9b797638415f72d
father <1359068985@qq.com>
2018-12-28 02:43:03 +0800

    小改动

--- src/main/java/com/rest/websocket/WebSocketConfig.java
+++ src/main/java/com/rest/websocket/WebSocketConfig.java
@@ -1,11 +1,13 @@
 package com.rest.websocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -29,6 +31,9 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/webServer").withSockJS();
         registry.addEndpoint("/queueServer").withSockJS();
     }
-
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter() {
+//        return new ServerEndpointExporter();
+//    }
 
 }
