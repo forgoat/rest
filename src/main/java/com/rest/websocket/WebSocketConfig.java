4553f36b1f903d2b0f06b38573defc7203f68c36
Lin Geng <20420162201666@stu.xmu.edu.cn>
2018-12-28 02:42:50 +0800

    WebSocket接受信息

--- src/main/java/com/rest/websocket/WebSocketConfig.java
+++ src/main/java/com/rest/websocket/WebSocketConfig.java
@@ -1,34 +1,39 @@
 package com.rest.websocket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-@EnableWebSocketMessageBroker
-@MessageMapping
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-    /**
-     * topic用来广播，user用来实现p2p
-     * @param config
-     */
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic","/user");
+//@EnableWebSocketMessageBroker
+//@MessageMapping
+public class WebSocketConfig{
+//    /**
+//     * topic用来广播，user用来实现p2p
+//     * @param config
+//     */
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        config.enableSimpleBroker("/topic","/user");
+//    }
+//
+//    /**
+//     * //注册两个STOMP的endpoint，分别用于广播和点对点
+//     * @param registry
+//     */
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/webServer").withSockJS();
+//        registry.addEndpoint("/queueServer").withSockJS();
+//    }
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
     }
-
-    /**
-     * //注册两个STOMP的endpoint，分别用于广播和点对点
-     * @param registry
-     */
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/webServer").withSockJS();
-        registry.addEndpoint("/queueServer").withSockJS();
-    }
-
 
 }
