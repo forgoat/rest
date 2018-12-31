3d28971b1b64bf6bff839dcf4b3dc6e2a86f59e1
Lin Geng <20420162201666@stu.xmu.edu.cn>
2018-12-31 10:17:38 +0800

    修改轮次成绩，由course加权

--- src/main/java/com/rest/controller/AdminController.java
+++ src/main/java/com/rest/controller/AdminController.java
@@ -2,16 +2,12 @@ package com.rest.controller;
 
 import com.rest.entity.Admin;
 import com.rest.service.AdminService;
-import lombok.AllArgsConstructor;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.math.BigInteger;
-import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -28,7 +24,7 @@ public class   AdminController {
         return adminService.findAdminById(id);
     }
 
-    @GetMapping(value = "/login")
+    @PostMapping(value = "/login")
     public HttpStatus login(String account, String password){
         System.out.print(account+password);
         return adminService.adminLogin(account,password);
