1af8cb5bdf92908c119c625abf877f0203d8fe49
Lin Geng <20420162201666@stu.xmu.edu.cn>
2019-01-02 01:03:15 +0800

    批量添加

--- src/main/java/com/rest/dao/TeamStudentDao.java
+++ src/main/java/com/rest/dao/TeamStudentDao.java
@@ -9,11 +9,10 @@ import java.util.List;
 @Mapper
 public interface TeamStudentDao {
     Long findByStudentId(@Param("studentId") Long studentId);
-
     List<Long> queryByTeamId(Long teamId);
+    public int save(TeamStudent teamStudent);
     public int deleteByTeamId(Long teamId);
     public List<TeamStudent> findByTeamId(Long teamId);
-
     int batchInsertTeamStudent(@Param("studentIdList") List<Long> studentIdList,@Param("teamId")Long teamId);
     int deleteByStudentId(@Param("studentId") Long studentId);
 
